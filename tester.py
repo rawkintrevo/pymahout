92d7746c72ec2e62c82adc910e395b9b75bb4fcb
rawkintrevo <trevor.d.grant@gmail.com>
2021-09-01 10:04:52 -0500

    mahout context

--- tester.py
+++ tester.py
@@ -1,15 +1,8 @@
 import pyspark
-#
-# version = "14.2-SNAPHOT"
-# spark_jars_packages = ','.join(['org.apache.mahout:mahout-core:14.1',
-#                                 'org.apache.mahout:mahout-spark:jar:scala-2.11:14.1'])
-#
-# jars = ['https://repo1.maven.org/maven2/org/apache/mahout/mahout-spark/14.1/mahout-spark-14.1-scala_2.11.jar',
-#         'https://repo1.maven.org/maven2/org/apache/mahout/mahout-core/14.1/mahout-core-14.1-scala_2.11.jar']
 
 jars = ','.join([f'file:///opt/spark/jars/mahout-core-14.2-SNAPSHOT-scala_2.11.jar',
                  f'file:///opt/spark/jars/mahout-core-14.2-SNAPSHOT-scala_2.11.jar',
-                 f'file:///opt/spark/jars/apache-mahout-14.2-SNAPSHOT-dependency-reduced.jar'
+                 f'file:///opt/spark/jars/apache-mahout-14.2-SNAPSHOT-dependency-reduced.jar',
                  f'file:///opt/spark/jars/mahout-spark-14.2-SNAPSHOT-scala_2.11.jar',])
 
 spark_conf = pyspark.SparkConf()
@@ -25,9 +18,7 @@ spark_conf.setAll([
 
 sc = pyspark.SparkContext('local[*]', conf=spark_conf)
 
-# jvm = sc._gateway._jvm
-
-m = sc._gateway.jvm.org.apache.mahout.math.Matrices().uniformGenerator(1).asFormatString()
+# mc = sc._gateway.jvm.org.apache.mahout.math.Matrices().uniformGenerator(1).asFormatString()
 ## ^^ Method does not exist
 
 """
@@ -56,23 +47,14 @@ def ref_scala_object(object_name):
     o = ff.get(None)
     return o
 
-obj = ref_scala_object("org.apache.mahout.sparkbindings.SparkEngine")
 
 ## Mahout distributed context
-# https://github.com/apache/mahout/blob/ad56ec44705e907a3f7f7c9ee6132f83c9d4b74a/engine/spark/src/main/scala/org/apache/mahout/sparkbindings/package.scala#L62
-
-objectName = "org.apache.mahout.sparkbindings"
-mc = sc._gateway.jvm.java.lang.Class.forName(objectName+".package$").mahoutSparkContext(
-    "local[*]",
-    "pymahout-app",
-    None,
-    sc._conf,
-    True
-    )
-
-obj2 = ref_scala_object("org.apache.mahout.sparkbindings")
+mc = sc._gateway.jvm.org.apache.mahout.sparkbindings.SparkDistributedContext(sc._jsc.sc())
 
 
+# Parallelize DRM
+obj = ref_scala_object("org.apache.mahout.sparkbindings.SparkEngine")
+obj.drmParallelizeWithRowIndices(m1)
 # object_name = "org.apache.mahout.sparkbindings.SparkEngine"
 # clazz = sc._gateway.jvm.java.lang.Class.forName(object_name+"$")
 # clazz.getDeclaredField()
@@ -82,13 +64,13 @@ obj.getClass().getName() # returns 'org.apache.mahout.sparkbindings.SparkEngine$
 obj2 = ref_scala_object("org.apache.mahout.sparkbindings.SparkEngine")
 # obj.drmParallelizeEmpty(100,10) #method does not exist
 obj.get("hdfsUtils")
-drm2= sc._gateway.jvm.org.apache.mahout.sparkbindings.SparkEngine$drmParallelizeWithRowIndices(m1)
+drm= sc._gateway.jvm.org.apache.mahout.sparkbindings.SparkEngine$drmParallelizeWithRowIndices(m1)
 #org.apache.mahout.math.drm.drmParallelize
 ### Create an RDD
 sc._jvm.org.apache.mahout.sparkbindings.SparkEngine.drmParallelizeEmpty(100, 10)
 ### Wrap the RDD into a DRM
 
-=======
+
 """out of core"""
 
 # Returns the static object instance on the heap
